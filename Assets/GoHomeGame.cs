0921eaa663cbe563eea5ac6756ef135e86a84d33
Sam Pattuzzi <sam.pattuzzi@googlemail.com>
2016-03-17 21:38:03 +0000

    GH07 Scope and Context of Variables

--- Assets/GoHomeGame.cs
+++ Assets/GoHomeGame.cs
@@ -3,32 +3,39 @@ using System.Collections;
 
 public class GoHomeGame : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+    int numberOfSteps = 1;
+    float distance = 5.0f;
+
+    // Use this for initialization
+    void Start () {
         print("Welcome to Go Home!");
         print("A game where you need to find you way home.");
     }
 
     // Update is called once per frame
     void Update () {
-        int numberOfSteps = 10;
-        float distance = 0.0f;
 
         if (Input.GetKeyDown(KeyCode.DownArrow))
         {
             print("After " + numberOfSteps + " steps, you are " + distance + " meters away from home.");
+            numberOfSteps++;
         }
         else if (Input.GetKeyDown(KeyCode.UpArrow))
         {
             print("After " + numberOfSteps + " steps, you are " + distance + " meters away from home.");
+            numberOfSteps++;
         }
         else if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
+            distance--;
             print("After " + numberOfSteps + " steps, you are " + distance + " meters away from home.");
+            numberOfSteps++;
         }
         else if (Input.GetKeyDown(KeyCode.RightArrow))
         {
+            distance++;
             print("After " + numberOfSteps + " steps, you are " + distance + " meters away from home.");
+            numberOfSteps++;
         }
 
     }
