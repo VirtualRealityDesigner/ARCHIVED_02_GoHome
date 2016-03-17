c304c61ecd3286cb65ca3693b3ebd2646b38178c
Sam Pattuzzi <sam.pattuzzi@googlemail.com>
2016-03-17 15:58:19 +0000

    GH04 Using Variables

--- Assets/GoHomeGame.cs
+++ Assets/GoHomeGame.cs
@@ -5,8 +5,11 @@ public class GoHomeGame : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+        int numberOfSteps = 10;
+        float distance = 0.0f;
         print("Welcome to Go Home!");
         print("A game where you need to find you way home.");
+        print("After " + numberOfSteps + " steps, you are " + distance + " meters away from home.");
     }
 
     // Update is called once per frame
