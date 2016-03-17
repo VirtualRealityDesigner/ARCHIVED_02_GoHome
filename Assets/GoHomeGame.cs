29c70092e5c8dc9ea74fb48a1e19e61942a9423e
Sam Pattuzzi <sam.pattuzzi@googlemail.com>
2016-03-17 15:42:55 +0000

    GH03 Printing Text to the Console

---
+++ Assets/GoHomeGame.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GoHomeGame : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        print("Welcome to Go Home!");
+        print("A game where you need to find you way home.");
+    }
+
+    // Update is called once per frame
+    void Update () {
+	
+	}
+}
